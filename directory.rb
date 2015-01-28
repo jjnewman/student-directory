3efeb6ba5bbbb00d26bff938bc51e097c774f5e3
Joe Newman <jj_newman@hotmail.com>
2015-01-28 11:45:29 +0000

    Changed all 'puts' to 'print' and updated 'print' method name to avoid conflict

--- directory.rb
+++ directory.rb
@@ -30,8 +30,8 @@ students = [
 ]
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  print "Please enter the names of the students\n"
+  print "To finish, just hit return twice\n"
   #create an empty array
   students = []
   #gets the first name
@@ -40,7 +40,7 @@ def input_students
   while !name.empty? do
     #add the student hash to the array
     students << {:name => name, :cohort => :February}
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students\n"
     #get another name from the user
     name = gets.chomp
   end
@@ -49,22 +49,22 @@ def input_students
 end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+  print "The students of my cohort at Makers Academy\n"
+  print "-------------\n"
 end
 
-def print(names)
+def print_all(names)
   names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+    print "#{name[:name]} (#{name[:cohort]} cohort)\n"
   end
 end
   
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+  print "Overall, we have #{names.length} great students\n"
 end
 
 students = input_students
 print_header
-print(students)
+print_all(students)
 print_footer(students)
   
