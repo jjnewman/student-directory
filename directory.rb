7ab40eb39e4fd4284aedf4b355ea77e2c96f9a24
Joe Newman <jj_newman@hotmail.com>
2015-01-28 10:21:28 +0000

    Student hashes created and populated with cohort

--- directory.rb
+++ directory.rb
@@ -1,32 +1,32 @@
 #put students into an array
 students = [
-  "Christopher Ward",
-  "Rizwan Ali",
-  "Vanessa Virgitti",
-  "Tommaso Bratto",
-  "Joe Newman",
-  "Loris Fochesato",
-  "Guido Vita Finzi",
-  "Tom Coakes",
-  "Kevin Lanzon",
-  "Sebastien Pires",
-  "Yannick Brunner",
-  "Costas Sarris",
-  "Diego Romero",
-  "Erik Griffin",
-  "Thomas Strothjohann",
-  "Alex Parkinson",
-  "Carrie Christenson",
-  "Julian Veling",
-  "Edward Brown",
-  "Meads Chalcroft",
-  "Sean Haughton",
-  "Paul Fitzgerald",
-  "Munded",
-  "Phil Brockwell",
-  "Gabe",
-  "Kate Beavis",
-  "Louise"
+  {:name => "Christopher Ward", :cohort => :February},
+  {:name => "Rizwan Ali", :cohort => :February},
+  {:name => "Vanessa Virgitti", :cohort => :February},
+  {:name => "Tommaso Bratto", :cohort => :February},
+  {:name => "Joe Newman", :cohort => :February},
+  {:name => "Loris Fochesato", :cohort => :February},
+  {:name => "Guido Vita Finzi", :cohort => :February},
+  {:name => "Tom Coakes", :cohort => :February},
+  {:name => "Kevin Lanzon", :cohort => :February},
+  {:name => "Sebastien Pires", :cohort => :February},
+  {:name => "Yannick Brunner", :cohort => :February},
+  {:name => "Costas Sarris", :cohort => :February},
+  {:name => "Diego Romero", :cohort => :February},
+  {:name => "Erik Griffin", :cohort => :February},
+  {:name => "Thomas Strothjohann", :cohort => :February},
+  {:name => "Alex Parkinson", :cohort => :February},
+  {:name => "Carrie Christenson", :cohort => :February},
+  {:name => "Julian Veling", :cohort => :February},
+  {:name => "Edward Brown", :cohort => :February},
+  {:name => "Meads Chalcroft", :cohort => :February},
+  {:name => "Sean Haughton", :cohort => :February},
+  {:name => "Paul Fitzgerald", :cohort => :February},
+  {:name => "Munded", :cohort => :February},
+  {:name => "Phil Brockwell", :cohort => :February},
+  {:name => "Gabe", :cohort => :February},
+  {:name => "Kate Beavis", :cohort => :February},
+  {:name => "Louise", :cohort => :February}
 ]
 
 def print_header
@@ -36,7 +36,7 @@ end
 
 def print(names)
   names.each do |name|
-    puts name
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
   
