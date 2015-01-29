be2325f16c0ac67de76458a30b170f2389a127ab
Joe Newman <jj_newman@hotmail.com>
2015-01-29 10:54:20 +0000

    students.csv file created to save data

--- directory.rb
+++ directory.rb
@@ -40,6 +40,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"
 end
 
@@ -49,6 +50,8 @@ def process(selection)
       input_students  
       when "2"
       show_students    
+      when "3"
+      save_students
       when "9"
       exit
       else
@@ -91,6 +94,17 @@ def print_footer
   puts "Overall, we have #{@students.length} great students"
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 interactive_menu
 #students = input_students
